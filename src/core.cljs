6fb9b3ff9c16d2ae8c8f064bacff43ee229de0bd
Shundeev Egor <shundeevegor@gmail.com>
2021-09-21 23:55:58 +0300

    Restyle a little.

--- src/core.cljs
+++ src/core.cljs
@@ -1,54 +1,60 @@
 (ns core
-  (:require [cljs.core.async :as a]
-            [reagent.dom :as rdom]
-            [reagent.core :as r]
-            [clojure.string :as str]))
+  (:require
+    [clojure.string :as str]
+    [reagent.core :as r]
+    [reagent.dom :as rdom]))
+
 
 (def enter-key 13)
 
 (defonce !storage (r/atom []))
 
+
 (defn redo
-  [{:keys [storage] :as state}]
+  [state]
   (update state :storage
           #(reduce
-            (fn [storage {:keys [type] :as action}]
-              (let [{last-type :type} (last storage)]
-                (cond
-                  (and (= last-type :undo) (= type :redo)) (pop storage)
-                  :else (conj storage action)))) [] %)))
+             (fn [storage {:keys [type] :as action}]
+               (let [{last-type :type} (last storage)]
+                 (cond
+                   (and (= last-type :undo) (= type :redo)) (pop storage)
+                   :else (conj storage action)))) [] %)))
+
 
 (defn undo
-  [{:keys [storage] :as state}]
+  [state]
   (update state :storage
           #(reduce
-            (fn [storage {:keys [type] :as action}]
-              (cond
-                (and (seq storage) (= type :undo)) (pop storage)
-                :else (conj storage action))) [] %)))
+             (fn [storage {:keys [type] :as action}]
+               (cond
+                 (and (seq storage) (= type :undo)) (pop storage)
+                 :else (conj storage action))) [] %)))
+
 
 (defn add-items
   [{:keys [storage] :as state}]
   (reduce
-   (fn [{{:keys [idx]} :todo  :as state} {:keys [type entry] :as action}]
-     (let [idx (inc idx)]
-       (cond-> state
-         (= type :add-item)
-         (->
-          (assoc-in [:todo :idx] idx)
-          (update-in [:todo :list] conj {:idx idx :entry entry})))))
-   state storage))
+    (fn [{{:keys [idx]} :todo  :as state} {:keys [type entry] :as _action}]
+      (let [idx (inc idx)]
+        (cond-> state
+          (= type :add-item)
+          (->
+            (assoc-in [:todo :idx] idx)
+            (update-in [:todo :list] conj {:idx idx :entry entry})))))
+    state storage))
+
 
 (defn delete-items
   [{:keys [storage] :as state}]
   (reduce
-   (fn [state {:keys [type idx] :as action}]
-     (cond-> state
-       (= type :delete-item)
-       (update-in [:todo :list]
-                  (fn [list]
-                    (remove #(= (:idx %) idx) list)))))
-   state storage))
+    (fn [state {:keys [type idx] :as _action}]
+      (cond-> state
+        (= type :delete-item)
+        (update-in [:todo :list]
+                   (fn [list]
+                     (remove #(= (:idx %) idx) list)))))
+    state storage))
+
 
 (defn todo-list
   [storage]
@@ -61,15 +67,20 @@
       delete-items
       (get-in [:todo :list])))
 
-(defn dispatch! [action]
+
+(defn dispatch!
+  [action]
   (r/rswap! !storage conj action))
 
-(defn on-key-down [event]
+
+(defn on-key-down
+  [event]
   (let [key-pressed (.-which event)
         entry (.. event -target -value)]
     (when (and (= key-pressed enter-key)
                (not (str/blank? entry)))
       (dispatch! {:type :add-item :entry entry}))))
+
 
 (defn todo-input
   []
@@ -78,6 +89,7 @@
             :margin-bottom "25px"}
     :placeholder "What needs to be done?"
     :on-key-down on-key-down}])
+
 
 (defn undo-button
   []
@@ -88,6 +100,7 @@
     :on-click #(dispatch! {:type :undo})}
    "Undo"])
 
+
 (defn redo-button
   []
   [:button.button
@@ -96,8 +109,9 @@
     :on-click #(dispatch! {:type :redo})}
    "Redo"])
 
+
 (defn todo-item
-  [{:keys [id entry on-delete] :as props}]
+  [{:keys [id entry] :as _props}]
   [:div.is-flex.is-justify-content-space-between.is-align-items-baseline.todo-item
    {:id id}
    [:div
@@ -109,7 +123,9 @@
     {:on-click #(dispatch! {:type :delete-item :idx id})}
     [:div.delete-item "🗑"]]])
 
-(defn main-page []
+
+(defn main-page
+  []
   (let [todo-list @(r/track! todo-list @!storage)]
     [:div.is-flex.is-justify-content-center
      [:div.app
@@ -122,6 +138,7 @@
          ^{:key idx}
          [todo-item {:id idx :entry entry}])]]]))
 
+
 (rdom/render
- [main-page]
- (.getElementById js/document "app"))
+  [main-page]
+  (.getElementById js/document "app"))
